a2ef22ced452480404370a438db85ed0afa74514
Daehoon Kim <ghsdh3409@gmail.com>
2021-02-10 14:15:41 +0900

    Sync master branch with the original repo (#7)
    * Allow to override dynamic tooltip styles
    
    Since tooltip bubble width, heigh and position are dynamically
    calculated, it doesn't allow to make it statically sized, due to this
    it was not possible to change the bubble to take has much width as
    possible.
    This became clear when the highlighted element was in the middle of the
    screen making the bubble be confined to less than half of the screen, so
    with some considerable amount of text, it would look rather weird.
    
    Signed-off-by: Marcos Loureiro <mloureiro.dev@gmail.com>
    
    * Pass useNativeDriver: false to all Animated.timing calls.
    
    * chore(package): Bump version to 2.4.8
    
    * fix declaration
    
    * fix readme
    
    * Adding an 'arrowColor' property to change the color of the arrow in the tooltip.
    
    * Updating README to reflect the new arrowColor property
    
    * Bump version to 2.5.0
    
    * Create FUNDING.yml
    
    * Bump version 2.5.1
    
    * Allow custom svgMask for each step (#203)
    
    * add step data to svg path func
    
    * improve readme
    
    * fix readme
    
    Co-authored-by: Domantas Bernatavicius <domantas@cornercasetech.com>
    
    Co-authored-by: Marcos Loureiro <mloureiro.dev@gmail.com>
    Co-authored-by: Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
    Co-authored-by: Mohamad Mohebifar <mohebifar@users.noreply.github.com>
    Co-authored-by: Ryotaro Namba <allscience1023@gmail.com>
    Co-authored-by: harrydoerr <harrydstephens@gmail.com>
    Co-authored-by: Domantas <domantas.bernatavicius@gmail.com>
    Co-authored-by: Domantas Bernatavicius <domantas@cornercasetech.com>

--- src/hocs/copilot.js
+++ src/hocs/copilot.js
@@ -43,6 +43,7 @@ const copilot = ({
   svgMaskPath,
   verticalOffset = 0,
   wrapperStyle,
+  arrowColor,
 } = {}) =>
   (WrappedComponent) => {
     class Copilot extends Component<any, State> {
@@ -228,6 +229,7 @@ const copilot = ({
               backdropColor={backdropColor}
               svgMaskPath={svgMaskPath}
               stopOnOutsideClick={stopOnOutsideClick}
+              arrowColor={arrowColor}
               ref={(modal) => { this.modal = modal; }}
             />
           </View>
